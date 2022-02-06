f37430df131526f3119a767ad5b0a2f003c3e628
Rudy Eko Prasetya <rudyekoprasetya@yahoo.co.id>
2022-02-06 18:34:29 +0700

    [FIX] query untuk export lagu

--- src/PlaylistsService.js
+++ src/PlaylistsService.js
@@ -7,9 +7,7 @@ class PlaylistsService {
 
   async getPlaylist(userId) {
     const query = {
-      text: `SELECT songs.id, songs.title, songs.performer FROM songs
-      LEFT JOIN playlistsongs ON songs.id = playlistsongs.song_id
-      WHERE playlistsongs.playlist_id = $1`,
+      text: `SELECT a.id, a.title, a.performer FROM songs a LEFT JOIN playlistsongs b ON a.id = b.song_id LEFT JOIN playlists c ON b.playlist_id = c.id WHERE c.owner= $1`,
       values: [userId],
     };
     const result = await this._pool.query(query);
